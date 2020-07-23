c6c81be6898f5f269e521a1ef559b9df39336edd
Chvostal Frantisek <ferochvostal@gmail.com>
2020-07-24 00:20:35 +0200

    polyfill to support mainly fetch in >=IE11

--- SettleChat/ClientApp/src/index.tsx
+++ SettleChat/ClientApp/src/index.tsx
@@ -1,4 +1,6 @@
+import 'react-app-polyfill/ie11'; // this must be the first line
 import 'bootstrap/dist/css/bootstrap.css';
+
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
