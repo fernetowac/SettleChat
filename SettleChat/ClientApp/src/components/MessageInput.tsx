3dfdbe2a2015a073bfab28cfa1832f1074ee9c38
Chvostal Frantisek <ferochvostal@gmail.com>
2020-11-07 21:53:17 +0100

    Displaying users currently writing

--- SettleChat/ClientApp/src/components/MessageInput.tsx
+++ SettleChat/ClientApp/src/components/MessageInput.tsx
@@ -6,13 +6,9 @@ import { ApplicationState } from '../store/index';
 import { HttpFailStatusReceivedAction } from '../actions/HttpStatusActions';
 import { usePrevious } from '../hooks/usePrevious';
 
-type MessageInputPropsStateType = {
-    userNamesCurrentlyWriting: string[]
-}
-type MessageInputProps = MessageInputPropsStateType & MapDispatchToPropsType & ConversationStore.Message;
 const writingActivityNotificationThresholdMiliseconds = 10 * 1000;
 
-function MessageInput(props: MessageInputProps) {
+function MessageInput(props: MapDispatchToPropsType) {
     const [inputMessage, setInputMessage] = React.useState('');
     const previousInputMessage = usePrevious(inputMessage);
     const [writingActivity, setWritingActivity] = React.useState({ activity: ConversationStore.WritingActivity.StoppedWriting, lastChange: new Date() } as ConversationStore.WritingActivityData);
@@ -23,8 +19,7 @@ function MessageInput(props: MessageInputProps) {
         props.actions.addMessage(inputMessage);
         setInputMessage('');
     };
-    //TODO: if writing, post writing activity regularly to server (to notify people who opened conversation after my start of writing)
-    //TODO: display only when writing activity of other people is new enough (to prevent being stuck in "John is writing.." status when there are connection problems on my side or other people's side)
+
     const handleWritingActivity = (inputValue: string): void => {
         if (timer) {
             window.clearTimeout(timer);
@@ -59,22 +54,10 @@ function MessageInput(props: MessageInputProps) {
         }
     }, [inputMessage]);
 
-    const renderUsersCurrentlyWriting = (userNamesCurrentlyWriting: string[]) => {
-        switch (userNamesCurrentlyWriting.length) {
-            case 0:
-                return '';
-            case 1:
-                return `${userNamesCurrentlyWriting[0]} is writing..`;
-            default:
-                return `${userNamesCurrentlyWriting.join(', ')} are writing..`;
-        }
-    }
-
     return (
         <React.Fragment>
             <form onSubmit={handleSubmit}>
                 <div>writing activity: {writingActivity.activity}</div>
-                <div>{renderUsersCurrentlyWriting(props.userNamesCurrentlyWriting)}</div>
                 <label>
                     Message:
                     <input type="text" value={inputMessage} onChange={inputMessageOnChange} />
@@ -92,34 +75,6 @@ type MapDispatchToPropsType = {
     }
 };
 
-// Selects which state properties are merged into the component's props
-const mapStateToProps = (state: ApplicationState): MessageInputPropsStateType => {
-    if (!state.identity) {
-        throw new Error('Identity not initialized');
-    }
-    if (!state.identity.userId) {
-        throw new Error('UserId not initialized');
-    }
-    if (!state.conversation) {
-        throw new Error('Conversation not initialized');
-    }
-    const getUserById = (
-        (conversationUsers: ConversationStore.User[]) =>
-            (userId: string) => conversationUsers.find(user => user.id === userId)
-    )
-        (state.conversation.users);
-    return {
-        userNamesCurrentlyWriting: state.conversation.writingActivities
-            .filter(writingActivity => writingActivity.userId !== state.identity.userId && writingActivity.activity === ConversationStore.WritingActivity.IsWriting)
-            .map(
-                writingActivity => {
-                    const writingActivityUser =
-                        getUserById(writingActivity.userId);
-                    return writingActivityUser ? writingActivityUser.userName : '';
-                })
-    };
-};
-
 const mapDispatchToProps = (dispatch: ThunkDispatch<ApplicationState, undefined, ConversationStore.ConversationSendWritingActivity | HttpFailStatusReceivedAction>): MapDispatchToPropsType => ({
     actions: {
         updateWritingActivity: (writingActivity: ConversationStore.WritingActivityData): Promise<void> =>
@@ -132,6 +87,6 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<ApplicationState, undefined,
 });
 
 export default connect(
-    mapStateToProps,
+    undefined,
     mapDispatchToProps
 )(MessageInput as any);
---
+++ SettleChat/ClientApp/src/components/OthersWritingActivity.tsx
@@ -0,0 +1,123 @@
+﻿import * as React from 'react';
+import { connect } from 'react-redux';
+import * as ConversationStore from "../store/Conversation";
+import { ApplicationState } from '../store/index';
+
+type MessageInputPropsStateType = {
+    othersWriting: UserWriting[]
+}
+
+interface UserWriting {
+    name: string;
+    lastTimeWriting: Date;
+}
+
+const writingActivityNotificationThresholdMiliseconds = 10 * 1000;
+
+function equalArrays(array1: string[], array2: string[]) {
+    if (array1.length !== array2.length) {
+        return false;
+    }
+
+    array1 = array1.slice();
+    array1.sort();
+    array2 = array2.slice();
+    array2.sort();
+
+    for (var i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Displays writing activity of other people if it is new enough (to prevent being stuck in "John is writing.." status when there are connection problems on my side or other people's side)
+ * @param props
+ */
+function OthersWritingActivity(props: MessageInputPropsStateType) {
+    const [userNamesWriting, setUserNamesWriting] = React.useState(new Array<string>());
+    const [othersWritingTimer, setOthersWritingTimer] = React.useState<ReturnType<typeof setTimeout>>();
+
+    const getUserNamesCurrentlyWriting = (usersWriting: UserWriting[]): string[] => {
+        return usersWriting
+            .filter(x => x.lastTimeWriting.getTime() >= new Date().getTime() - writingActivityNotificationThresholdMiliseconds)
+            .map(x => x.name);
+    }
+
+    const resetUsersWritingIfNeeded = (unfilteredOthersWriting: UserWriting[]): string[] => {
+        const userNamesCurrentlyWriting = getUserNamesCurrentlyWriting(unfilteredOthersWriting);
+        if (!equalArrays(userNamesWriting, userNamesCurrentlyWriting)) {
+            console.debug(userNamesWriting);
+            console.debug(userNamesCurrentlyWriting);
+            setUserNamesWriting(userNamesCurrentlyWriting);
+        }
+        return userNamesCurrentlyWriting;
+    }
+
+    React.useEffect(() => {
+        const userNamesCurrentlyWriting = resetUsersWritingIfNeeded(props.othersWriting);
+        if (userNamesCurrentlyWriting.length) {
+            setOthersWritingTimer(setInterval(() => resetUsersWritingIfNeeded(props.othersWriting), 500));
+        }
+        return () => {
+            console.debug('MessageInput useEffect[props.othersWriting,othersWriting] cleanup');
+            if (othersWritingTimer) {
+                window.clearTimeout(othersWritingTimer);
+                setOthersWritingTimer(undefined);
+            }
+        }
+    }, [props.othersWriting, userNamesWriting]);
+
+    const renderUsersCurrentlyWriting = (userNamesCurrentlyWriting: string[]) => {
+        switch (userNamesCurrentlyWriting.length) {
+            case 0:
+                return '';
+            case 1:
+                return `${userNamesCurrentlyWriting[0]} is writing..`;
+            default:
+                return `${userNamesCurrentlyWriting.join(', ')} are writing..`;
+        }
+    }
+
+    return (
+        <React.Fragment>{renderUsersCurrentlyWriting(userNamesWriting)}</React.Fragment>
+    );
+}
+
+// Selects which state properties are merged into the component's props
+const mapStateToProps = (state: ApplicationState): MessageInputPropsStateType => {
+    if (!state.identity) {
+        throw new Error('Identity not initialized');
+    }
+    if (!state.identity.userId) {
+        throw new Error('UserId not initialized');
+    }
+    if (!state.conversation) {
+        throw new Error('Conversation not initialized');
+    }
+    const getUserById = (
+        (conversationUsers: ConversationStore.User[]) =>
+            (userId: string) => conversationUsers.find(user => user.id === userId)
+    )
+        (state.conversation.users);
+    return {
+        othersWriting: state.conversation.writingActivities
+            .filter(writingActivity =>
+                writingActivity.userId !== state.identity.userId
+                && writingActivity.activity === ConversationStore.WritingActivity.IsWriting
+                && writingActivity.lastChange.getTime() >= new Date().getTime() - writingActivityNotificationThresholdMiliseconds)
+            .map(
+                writingActivity => {
+                    const writingActivityUser = getUserById(writingActivity.userId);
+                    return {
+                        name: writingActivityUser ? writingActivityUser.userName : '',
+                        lastTimeWriting: writingActivity.lastChange
+                    }
+                })
+    };
+};
+
+export default connect(mapStateToProps)(OthersWritingActivity as any);
