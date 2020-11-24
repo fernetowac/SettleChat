5e02816f5b9d3057ea251f108b55ee81b52b2625
Chvostal Frantisek <ferochvostal@gmail.com>
2020-11-24 18:29:07 +0100

    enabled redux trace in development

--- SettleChat/ClientApp/src/store/configureStore.ts
+++ SettleChat/ClientApp/src/store/configureStore.ts
@@ -5,6 +5,7 @@ import { History } from 'history';
 import { ApplicationState, reducers } from './index';
 import { createIdentityMiddleware as createOIDCMiddleware } from '../middlewares/IdentityMiddleware';
 import { createConversationMiddleware } from '../middlewares/ConversationMiddleware';
+import { isDevelopment } from '../helpers/development/DevDetect';
 
 export default function configureStore(history: History, initialState: ApplicationState) {
     const middleware = [
@@ -22,7 +23,8 @@ export default function configureStore(history: History, initialState: Applicati
     const enhancers = [];
     const windowIfDefined = typeof window === 'undefined' ? null : window as any;
     if (windowIfDefined && windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__) {
-        enhancers.push(windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__());
+        const reduxDevToolsSettings = isDevelopment ? { trace: true, traceLimit: 25 } : {};
+        enhancers.push(windowIfDefined.__REDUX_DEVTOOLS_EXTENSION__(reduxDevToolsSettings));
     }
 
     return createStore(
